f11fdffd5dcd64f21aeda1364240bd83a5ddc147
MateusGaioffatto <gaioffattomateus@gmail.com>
2025-08-16 20:49:18 -0300

    nice

--- CodeFiles/selectPokemonTypesFile.js
+++ CodeFiles/selectPokemonTypesFile.js
@@ -1,384 +1,123 @@
 effectTable.addEventListener('mouseover', function() {learnMoreAboutText.style.display = "inline";}) // TRUE = DISPLAY: "INLINE"
 effectTable.addEventListener('mouseout', function() {learnMoreAboutText.style.display = "none";}) // FALSE = DISPLAY: "NONE"
 
+// DEFINIÇÃO DE CONSTANTES: 
+// const [typeName]Effects = [weakEffecs, resistancesEffects, nullEffects, noDamageEffects, superEffectiveEffects, notVeryEffectiveEffects]
+
 // POKEMON TYPES: FUNCTIONS
 
-// NORMAL TYPE:
-selectTypesButtons[0].addEventListener("click", function() {
+// selectTypesButtons.forEach(selectButton => {
 
-    typeSelectedCount[doubleClick] = selectTypesButtons[0];
+//     selectButton.addEventListener('click', function() {
+
+//     })
+
+// })
+
+// NORMAL TYPE: selectType NODELIST FUNCTION
+selectTypesButtons[0].addEventListener('click', function() {
+    const normalEffects = [[6], [], [13], [13], [], [12,16]];
+    showTypesEffects(0, normalEffects);
+})
+// FIRE TYPE: selectType NODELIST FUNCTION
+selectTypesButtons[1].addEventListener('click', function() {
+    const fireEffects = [[2, 8, 12], [1, 4, 5, 11, 16, 17], [], [], [4, 5, 11, 16], [1, 2, 12, 14]];
+    showTypesEffects(1, fireEffects);
+})
+// WATER TYPE: selectType NODELIST FUNCTION
+selectTypesButtons[2].addEventListener('click', function() {
+    const waterEffects = [[3, 4], [1, 2, 5, 16], [], [], [1, 8, 12], [2, 4, 14]];
+    showTypesEffects(2, waterEffects);
+})
+// ELECTRIC TYPE: selectType NODELIST FUNCTION
+selectTypesButtons[3].addEventListener('click', function() {
+    const electricEffects = [[8], [3, 9, 16], [], [8], [2, 9], [3, 4, 14]];
+    showTypesEffects(3, electricEffects);
+})
+// GRASS TYPE: selectType NODELIST FUNCTION
+selectTypesButtons[4].addEventListener('click', function() {
+    const grassEffects = [[1, 5, 7, 9, 11], [2, 3, 4, 8], [], [], [2, 8, 12], [1, 4, 7, 9, 11, 14, 16]];
+    showTypesEffects(4, grassEffects);
+})
+// ICE TYPE: selectType NODELIST FUNCTION
+selectTypesButtons[5].addEventListener('click', function() {
+    const iceEffects = [[1, 6, 12, 16], [5], [], [], [4, 8, 9, 14], [1, 2, 5, 16]];
+    showTypesEffects(5, iceEffects);
+})
+// FIGHTING TYPE: selectType NODELIST FUNCTION
+selectTypesButtons[6].addEventListener('click', function() {
+    const fightingEffects = [[9, 10, 17], [11, 12, 15], [], [13], [0, 5, 12, 15, 17], [7, 9, 10, 11, 17]];
+    showTypesEffects(6, fightingEffects);
+})
+// POISON TYPE: selectType NODELIST FUNCTION
+selectTypesButtons[7].addEventListener('click', function() {
+    const poisonEffects = [[8, 10], [4, 6, 7, 11, 17], [], [16], [4, 17], [7, 8, 12, 13]];
+    showTypesEffects(7, poisonEffects);
+})
+// GROUND TYPE: selectType NODELIST FUNCTION
+selectTypesButtons[8].addEventListener('click', function() {
+    const groundEffects = [[2, 4, 5], [7, 12], [], [9], [1, 3, 7, 12, 16], [4, 9]];
+    showTypesEffects(8, groundEffects);
+})
+// FLYING TYPE: selectType NODELIST FUNCTION
+selectTypesButtons[9].addEventListener('click', function() {
+    const flyingEffects = [[3, 5, 12], [4, 6, 11], [], [], [4, 6, 11], [3, 8, 12, 16]];
+    showTypesEffects(9, flyingEffects);
+})
+// PSYCHIC TYPE: selectType NODELIST FUNCTION
+selectTypesButtons[10].addEventListener('click', function() {
+    const psychicEffects = [[11, 13, 15], [6, 10], [], [15], [6, 7], [10, 16]];
+    showTypesEffects(10, psychicEffects);
+})
+// BUG TYPE: selectType NODELIST FUNCTION
+selectTypesButtons[11].addEventListener('click', function() {
+    const bugEffects = [[1, 9, 12], [4, 6, 8], [], [], [4, 10, 15], [1, 6, 7, 9, 11, 13, 16, 17]];
+    showTypesEffects(11, bugEffects);
+})
+// ROCK TYPE: selectType NODELIST FUNCTION
+selectTypesButtons[12].addEventListener('click', function() {
+    const rockEffects = [[2, 4, 6, 8, 16], [0, 1, 7, 9], [], [], [1, 5, 9, 11], [6, 7, 8, 12, 16]];
+    showTypesEffects(12, rockEffects);
+})
+// GHOST TYPE: selectType NODELIST FUNCTION
+selectTypesButtons[13].addEventListener('click', function() {
+    const ghostEffects = [[13, 15], [6, 7, 11], [], [0], [10, 13], [15]];
+    showTypesEffects(13, ghostEffects);
+})
+// DRAGON TYPE: selectType NODELIST FUNCTION
+selectTypesButtons[14].addEventListener('click', function() {
+    const dragonEffects = [[5, 14, 17], [1, 2, 3, 4], [], [17], [2, 3, 4, 5, 11, 14], [16]];
+    showTypesEffects(14, dragonEffects);
+})
+// DARK TYPE: selectType NODELIST FUNCTION
+selectTypesButtons[15].addEventListener('click', function() {
+    const darkEffects = [[6, 11, 17], [13, 15], [], [], [10, 13], [6, 15, 17]];
+    showTypesEffects(15, darkEffects);
+})
+// STEEL TYPE: selectType NODELIST FUNCTION
+selectTypesButtons[16].addEventListener('click', function() {
+    const steelEffects = [[1, 6, 8], [0, 4, 5, 7, 9, 10, 11, 12, 14, 16, 17], [], [], [5, 12, 14, 17], [1, 2, 3, 8, 9, 10, 11, 15, 16]];
+    showTypesEffects(16, steelEffects);
+})
+// FAIRY TYPE: selectType NODELIST FUNCTION
+selectTypesButtons[17].addEventListener('click', function() {
+    const fairyEffects = [[7, 16], [6, 11, 15], [14], [], [6, 14, 15], [1, 7, 16]];
+    showTypesEffects(17, fairyEffects);
+})
+
+function showTypesEffects(typeIndex,typeEffects) {
+    typeSelectedCount[doubleClick] = selectTypesButtons[typeIndex];
     doubleClick++;
 
-    if (typeSelectedCount[0] === typeSelectedCount[1] || doubleClick > 2) {
-        hideAllEffectsButtons();
-    }
+    if (typeSelectedCount[0] === typeSelectedCount[1] || doubleClick > 2) {hideAllEffectsButtons();}
     else {
         checkEffectsTitle();
         
-        createWeakEffects([6]);
-        createResistences([]);
-        createNullEffects([13]);
-        createNoDamageEffects([13]);
-        createSuperEffects([]);
-        createNotVeryEffective([12, 16]);
+        createWeakEffects(typeEffects[0]);
+        createResistences(typeEffects[1]);
+        createNullEffects(typeEffects[2]);
+        createNoDamageEffects(typeEffects[3]);
+        createSuperEffects(typeEffects[4]);
+        createNotVeryEffective(typeEffects[5]);
     }
-})
-
-// FIRE TYPE:
-selectTypesButtons[1].addEventListener("click", function() {
-
-    typeSelectedCount[doubleClick] = selectTypesButtons[1];
-    doubleClick++;
-
-    if (typeSelectedCount[0] === typeSelectedCount[1] || doubleClick > 2) {
-        hideAllEffectsButtons();
-    }
-    else {
-        checkEffectsTitle();
-        
-        createWeakEffects([2, 8, 12]);
-        createResistences([1, 4, 5, 11, 16, 17]);
-        createNullEffects([]);
-        createSuperEffects([4, 5, 11, 16]);
-        createNotVeryEffective([1, 2, 12, 14]);
-
-    }
-})
-
-// WATER TYPE:
-selectTypesButtons[2].addEventListener("click", function() {
-
-    typeSelectedCount[doubleClick] = selectTypesButtons[2];
-    doubleClick++;
-
-    if (typeSelectedCount[0] === typeSelectedCount[1] || doubleClick > 2) {
-        hideAllEffectsButtons();
-    }
-    else {
-        checkEffectsTitle();
-
-        createWeakEffects([3, 4]);
-        createResistences([1, 2, 5, 16]);
-        createNullEffects([]);
-        createSuperEffects([1, 8, 12]);
-        createNotVeryEffective([2, 4, 14]);
-    }
-})
-
-// ELECTRIC TYPE:
-selectTypesButtons[3].addEventListener("click", function() {
-
-
-    typeSelectedCount[doubleClick] = selectTypesButtons[3];
-    doubleClick++;
-
-    if (typeSelectedCount[0] === typeSelectedCount[1] || doubleClick > 2) {
-        hideAllEffectsButtons();
-    }
-    else {
-        checkEffectsTitle();
-
-        createWeakEffects([8]);
-        createResistences([3, 9, 16]);
-        createNullEffects([]);
-        createNoDamageEffects([8])
-        createSuperEffects([2, 9]);
-        createNotVeryEffective([3, 4, 14]);
-    }
-})
-
-// GRASS TYPE:
-selectTypesButtons[4].addEventListener("click", function() {
-
-    typeSelectedCount[doubleClick] = selectTypesButtons[4];
-    doubleClick++;
-    
-    if (typeSelectedCount[0] === typeSelectedCount[1] || doubleClick > 2) {
-        hideAllEffectsButtons();
-    }
-    else {
-        checkEffectsTitle();
-
-        createWeakEffects([1, 5, 7, 9, 11]);
-        createResistences([2, 3, 4, 8]);
-        createNullEffects([]);
-        createSuperEffects([2, 8, 12]);
-        createNotVeryEffective([1, 4, 7, 9, 11, 14, 16]);
-    }
-})
-
-// ICE TYPE:
-selectTypesButtons[5].addEventListener("click", function() {
-
-    typeSelectedCount[doubleClick] = selectTypesButtons[5];
-    doubleClick++;
-    if (typeSelectedCount[0] === typeSelectedCount[1] || doubleClick > 2) {
-        hideAllEffectsButtons();
-    }
-    else {
-        checkEffectsTitle();
-
-        createWeakEffects([1, 6, 12, 16]);
-        createResistences([5]);
-        createNullEffects([]);
-        createSuperEffects([4, 8, 9, 14]);
-        createNotVeryEffective([1, 2, 5, 16]);
-    }
-})
-
-// FIGHTING TYPE:
-selectTypesButtons[6].addEventListener("click", function() {
-
-
-    typeSelectedCount[doubleClick] = selectTypesButtons[6];
-    doubleClick++;
-
-    if (typeSelectedCount[0] === typeSelectedCount[1] || doubleClick > 2) {
-        hideAllEffectsButtons();
-    }
-    else {
-        checkEffectsTitle();
-
-        createWeakEffects([9, 10, 17]);
-        createResistences([11, 12, 15]);
-        createNullEffects([]);
-        createNoDamageEffects([13]);
-        createSuperEffects([0, 5, 12, 15, 17]);
-        createNotVeryEffective([7, 9, 10, 11, 17]);
-    }
-})
-
-// POISON TYPE:
-selectTypesButtons[7].addEventListener("click", function() {
-
-
-    typeSelectedCount[doubleClick] = selectTypesButtons[7];
-    doubleClick++;
-
-    if (typeSelectedCount[0] === typeSelectedCount[1] || doubleClick > 2) {
-        hideAllEffectsButtons();
-    }
-    else {
-        checkEffectsTitle();
-
-        createWeakEffects([8, 10]);
-        createResistences([4, 6, 7, 11, 17]);
-        createNullEffects([]);
-        createNoDamageEffects([16]);
-        createSuperEffects([4, 17]);
-        createNotVeryEffective([7, 8, 12, 13]);
-    }
-})
-
-// GROUND TYPE:
-selectTypesButtons[8].addEventListener("click", function() {
-
-
-    typeSelectedCount[doubleClick] = selectTypesButtons[8];
-    doubleClick++;
-
-    if (typeSelectedCount[0] === typeSelectedCount[1] || doubleClick > 2) {
-        hideAllEffectsButtons();
-    }
-    else {
-        checkEffectsTitle();
-
-        createWeakEffects([2, 4, 5]);
-        createResistences([7, 12]);
-        createNullEffects([]);
-        createNoDamageEffects([9]);
-        createSuperEffects([1, 3, 7, 12, 16]);
-        createNotVeryEffective([4, 9]);
-    }
-})
-
-// FLYING TYPE:
-selectTypesButtons[9].addEventListener("click", function() {
-
-    typeSelectedCount[doubleClick] = selectTypesButtons[9];
-    doubleClick++;
-
-    if (typeSelectedCount[0] === typeSelectedCount[1] || doubleClick > 2) {
-        hideAllEffectsButtons();
-    }
-    else {
-        checkEffectsTitle();
-
-        createWeakEffects([3, 5, 12]);
-        createResistences([4, 6, 11]);
-        createNullEffects([]);
-        createSuperEffects([4, 6, 11]);
-        createNotVeryEffective([3, 8, 12, 16]);
-    }
-})
-
-// PSYCHIC TYPE:
-selectTypesButtons[10].addEventListener("click", function() {
-
-
-    typeSelectedCount[doubleClick] = selectTypesButtons[10];
-    doubleClick++;
-
-    if (typeSelectedCount[0] === typeSelectedCount[1] || doubleClick > 2) {
-        hideAllEffectsButtons();
-    }
-    else {
-        checkEffectsTitle();
-
-        createWeakEffects([11, 13, 15]);
-        createResistences([6, 10]);
-        createNullEffects([]);
-        createNoDamageEffects([15]);
-        createSuperEffects([6, 7]);
-        createNotVeryEffective([10, 16]);
-    }
-})
-
-// BUG TYPE:
-selectTypesButtons[11].addEventListener("click", function() {
-
-
-    typeSelectedCount[doubleClick] = selectTypesButtons[11];
-    doubleClick++;
-
-    if (typeSelectedCount[0] === typeSelectedCount[1] || doubleClick > 2) {
-        hideAllEffectsButtons();
-    }
-    else {
-        checkEffectsTitle();
-
-        createWeakEffects([1, 9, 12]);
-        createResistences([4, 6, 8]);
-        createNullEffects([]);
-        createSuperEffects([4, 10, 15]);
-        createNotVeryEffective([1, 6, 7, 9, 11, 13, 16, 17]);
-    }
-})
-
-// ROCK TYPE:
-selectTypesButtons[12].addEventListener("click", function() {
-
-
-    typeSelectedCount[doubleClick] = selectTypesButtons[12];
-    doubleClick++;
-
-    if (typeSelectedCount[0] === typeSelectedCount[1] || doubleClick > 2) {
-        hideAllEffectsButtons();
-    }
-    else {
-        checkEffectsTitle();
-
-        createWeakEffects([2, 4, 6, 8, 16]);
-        createResistences([0, 1, 7, 9]);
-        createNullEffects([]);
-        createSuperEffects([1, 5, 9, 11]);
-        createNotVeryEffective([6, 7, 8, 12, 16]);
-    }
-})
-
-// GHOST TYPE:
-selectTypesButtons[13].addEventListener("click", function() {
-
-
-    typeSelectedCount[doubleClick] = selectTypesButtons[13];
-    doubleClick++;
-
-    if (typeSelectedCount[0] === typeSelectedCount[1] || doubleClick > 2) {
-        hideAllEffectsButtons();
-    }
-    else {
-        checkEffectsTitle();
-
-        createWeakEffects([13, 15]);
-        createResistences([6, 7, 11]);
-        createNullEffects([]);
-        createNoDamageEffects([0]);
-        createSuperEffects([10, 13]);
-        createNotVeryEffective([15]);
-    }
-})
-
-// DRAGON TYPE:
-selectTypesButtons[14].addEventListener("click", function() {
-
-
-    typeSelectedCount[doubleClick] = selectTypesButtons[14];
-    doubleClick++;
-
-    if (typeSelectedCount[0] === typeSelectedCount[1] || doubleClick > 2) {
-        hideAllEffectsButtons();
-    }
-    else {
-        checkEffectsTitle();
-
-        createWeakEffects([5, 14, 17]);
-        createResistences([1, 2, 3, 4]);
-        createNullEffects([]);
-        createNoDamageEffects([17]);
-        createSuperEffects([2, 3, 4, 5, 11, 14]);
-        createNotVeryEffective([16]);
-    }
-})
-
-// DARK TYPE:
-selectTypesButtons[15].addEventListener("click", function() {
-
-
-    typeSelectedCount[doubleClick] = selectTypesButtons[15];
-    doubleClick++;
-
-    if (typeSelectedCount[0] === typeSelectedCount[1] || doubleClick > 2) {
-        hideAllEffectsButtons();
-    }
-    else {
-        checkEffectsTitle();
-
-        createWeakEffects([6, 11, 17]);
-        createResistences([13, 15]);
-        createNullEffects([]);
-        createSuperEffects([10, 13]);
-        createNotVeryEffective([6, 15, 17]);
-    }
-})
-
-// STEEL TYPE:
-selectTypesButtons[16].addEventListener("click", function() {
-
-
-    typeSelectedCount[doubleClick] = selectTypesButtons[16];
-    doubleClick++;
-
-    if (typeSelectedCount[0] === typeSelectedCount[1] || doubleClick > 2) {
-        hideAllEffectsButtons();
-    }
-    else {
-        checkEffectsTitle();
-
-        createWeakEffects([1, 6, 8]);
-        createResistences([0, 4, 5, 7, 9, 10, 11, 12, 14, 16, 17]);
-        createNullEffects([]);
-        createSuperEffects([5, 12, 14, 17]);
-        createNotVeryEffective([1, 2, 3, 8, 9, 10, 11, 15, 16]);
-    }
-})
-
-// FAIRY TYPE:
-selectTypesButtons[17].addEventListener("click", function() {
-
-
-    typeSelectedCount[doubleClick] = selectTypesButtons[17];
-    doubleClick++;
-
-    if (typeSelectedCount[0] === typeSelectedCount[1] || doubleClick > 2) {
-        hideAllEffectsButtons();
-    }
-    else {
-        checkEffectsTitle();
-
-        createWeakEffects([7, 16]);
-        createResistences([6, 11, 15]);
-        createNullEffects([14]);
-        createSuperEffects([6, 14, 15]);
-        createNotVeryEffective([1, 7, 16]);
-    }
-})
+}
